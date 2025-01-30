eea3c6aba10f68adcd36d0a1717098682fde10f3
csyq <xidicai067@gmail.com>
2025-01-31 01:15:52 +0800

    Change proxy_site to https://www.shenyandayi.com/

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ if [ $# -ge 1 ]; then
         path=$(echo -n $v2ray_id | tail -c 12)
     fi
 
-    proxy_site="https://cn.bing.com/"
+    proxy_site="https://www.shenyandayi.com/"
 
     echo -e "domain: ${domain}"
     echo -e "netstack: ${netstack}"
